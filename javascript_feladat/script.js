cf9de4ee3539aa28c0481250937440469e980e34
Bence Farkas <bence.farkas2003@gmail.com>
2025-04-06 19:50:37 +0200

    Implement readin collection data from data.json and display it dynamically.
    Also tweaked the content and the styling of the cards

--- javascript_feladat/script.js
+++ javascript_feladat/script.js
@@ -1,10 +1,28 @@
-let collectionsData;
+const collectionsElement = document.querySelector(".collections");
 window.onload = () => {
   fetch("./data.json")
     .then((response) => response.json())
     .then((data) => {
-      collectionsData = data;
-      console.log(collectionsData[0].id);
+      loadCards(data);
     })
     .catch((err) => alert("Hiba az adatok betöltése közben!"));
 };
+
+function loadCards(collections) {
+  for (collection of collections) {
+    collectionsElement.innerHTML += `
+            <div class="card" style="width: 18rem">
+          <img
+            src="./assets/placeholder.jpeg"
+            class="card-img-top"
+            alt="Thumbnail"
+          />
+          <div class="card-body">
+            <h5 class="card-title">${collection.name}</h5>
+            
+            <a href="#" class="btn btn-outline-primary open-btn">Megnyitás</a>
+          </div>
+        </div>
+        `;
+  }
+}
