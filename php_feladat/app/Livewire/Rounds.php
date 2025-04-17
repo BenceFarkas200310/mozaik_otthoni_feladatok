b1202512de952f44fe886fc7b09dcabec65691b0
Bence Farkas <bence.farkas2003@gmail.com>
2025-04-17 23:46:05 +0200

    Create add round function

--- php_feladat/app/Livewire/Rounds.php
+++ php_feladat/app/Livewire/Rounds.php
@@ -7,13 +7,29 @@ use App\Models\Round;
 
 class Rounds extends Component
 {
-
     public $rounds;
+    public $newRoundName;
+    public $competition;
 
     public function mount($competition)
     {
         $this->competition = $competition;
         $this->rounds = Round::where('competition_id', $this->competition->id)->get();
+    }
+
+    public function addRound()
+    {
+        $this->validate([
+            'newRoundName' => 'required|string|max:255',
+        ]);
+
+        $round = Round::create([
+            'name' => $this->newRoundName,
+            'competition_id' => $this->competition->id,
+        ]);
+
+        $this->rounds->push($round);
+        $this->newRoundName = '';
     }
 
     public function render()
