a6def6f52d6596db3212b6845c423e9c0e80fcb3
Bence Farkas <bence.farkas2003@gmail.com>
2025-04-20 19:46:47 +0200

    Add basic styling to register page
    Also imported bootstrap in all pages

--- php_feladat/resources/views/livewire/register.blade.php
+++ php_feladat/resources/views/livewire/register.blade.php
@@ -1,37 +1,38 @@
-<div>
-    <h2>Regisztráció</h2>
+<div class="container-md mt-5 ml-auto mr-auto col-5 col-sm-10">
+
+    <center><h2 class="mb-5">Regisztráció</h2></center>
     <form wire:submit.prevent="register">
         @csrf
-        <div>
-            <label for="name">Név</label>
-            <input type="text" id="name" wire:model="name">
+        <div class="row">
+            <label for="name" class="col-md form-label">Név:</label>
+            <input type="text" id="name" wire:model="name" class="col input-group-text mb-3">
             @error('name') <span class="error">{{ $message }}</span> @enderror
         </div>
 
-        <div>
-            <label for="email">Email</label>
-            <input type="email" id="email" wire:model="email">
+        <div class="row">
+            <label for="email" class="col-md form-label">Email:</label>
+            <input type="email" id="email" wire:model="email" class="col input-group-text mb-3">
             @error('email') <span class="error">{{ $message }}</span> @enderror
         </div>
 
-        <div>
-            <label for="password">Jelszó</label>
-            <input type="password" id="password" wire:model="password">
+        <div class="row">
+            <label for="password" class="col-md form-label">Jelszó:</label>
+            <input type="password" id="password" wire:model="password" class="col input-group-text mb-3">
             @error('password') <span class="error">{{ $message }}</span> @enderror
         </div>
 
-        <div>
-            <label for="phone_number">Telefonszám</label>
-            <input type="text" id="phone_number" wire:model="phone_number">
+        <div class="row">
+            <label for="phone_number" class="col-md form-label">Telefonszám:</label>
+            <input type="text" id="phone_number" wire:model="phone_number" class="col input-group-text mb-3" >
             @error('phone_number') <span class="error">{{ $message }}</span> @enderror
-        </div>
+        </div class="row">
 
-        <div>
-            <label for="address">Cím</label>
-            <input type="text" id="address" wire:model="address">
+        <div class="row">
+            <label for="address" class="col-md form-label">Cím:</label>
+            <input type="text" id="address" wire:model="address" class="col input-group-text mb-3">
             @error('address') <span class="error">{{ $message }}</span> @enderror
         </div>
 
-        <button type="submit">Regisztráció</button>
+        <center><button type="submit" class="btn btn-primary">Regisztráció</button></center>
     </form>
 </div>
--- php_feladat/resources/views/round-details.blade.php
+++ php_feladat/resources/views/round-details.blade.php
@@ -5,11 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Forduló részletei</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    @livewireStyles
 </head>
 <body>
     <h1>Forduló: {{ $round->name }}</h1>
     <h2>Versenyzők</h2>
     <livewire:add-contestant :round="$round->id" />
     <a href="/">Vissza a főoldalra</a>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    @livewireScripts
 </body>
 </html>
