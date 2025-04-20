877006af049fa687b5b07a16a8bf77720fb0ec7d
Bence Farkas <bence.farkas2003@gmail.com>
2025-04-20 19:53:38 +0200

    Add basic styling to login page

--- php_feladat/resources/views/livewire/login.blade.php
+++ php_feladat/resources/views/livewire/login.blade.php
@@ -1,19 +1,21 @@
-<div>
-    <h2>Bejelentkezés</h2>
+<div class="container-md mt-5 ml-auto mr-auto col-5 col-sm-10">
+    <center><h2 class="mb-5">Bejelentkezés</h2></center>
     <form wire:submit.prevent="login">
         @csrf
-        <div>
-            <label for="email-input">Email cím:</label>
-            <input type="text" name="email-input" wire:model="email">
+        <div class="row">
+            <label for="email-input" class="col-md form-label">Email cím:</label>
+            <input type="text" id="email-input" name="email-input" wire:model="email" class="col input-group-text mb-3">
             @error('email') <span class="error">{{ $message }}</span> @enderror
         </div>
 
-        <div>
-            <label for="password-input">Jelszó:</label>
-            <input type="password" name="email-input" wire:model="password">
+        <div class="row">
+            <label for="password-input" class="col-md form-label">Jelszó:</label>
+            <input type="password" id="password-input" name="email-input" wire:model="password" class="col input-group-text mb-3">
             @error('password') <span class="error">{{ $message }}</span> @enderror
         </div>
-        <button type="submit">Bejelentkezés</button>
-        <p>Nincs még fiókod? <a href="register">Regisztrálj</a></p>
+        <center>
+            <button type="submit" class="btn btn-primary">Bejelentkezés</button>
+            <p>Nincs még fiókod? <a href="register">Regisztrálj</a></p>
+        </center>
     </form>
 </div>
